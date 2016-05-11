fccfd346d42381b3ec807971fb508b7659d2aedd
mly <mly@localhost.localdomain>
2016-05-11 17:50:36 +0800

    add arrange and animation

--- components/component.js
+++ components/component.js
@@ -17,6 +17,7 @@ var ImageModule=React.createClass({
     ['WebkitTransform','MozTransform','msTransform','transform'].forEach(function(value){
       styleObj[value]='rotate('+t.props.arrange.rotate+')';
     });
+    styleObj.zIndex=this.props.arrange.isCenter?1:0;
     return (
       <figure className='img-figure' style={styleObj}>
         <img src={this.props.data.imgUrl} alt={this.props.data.title}/>
@@ -28,6 +29,25 @@ var ImageModule=React.createClass({
   }
 });
 
+var CtrlModule=React.createClass({
+  handleClick: function(e){
+    if(!this.props.isCenter){
+      this.props.reArrange();
+    }
+    e.preventDefault();
+    e.stopPropagation();
+  },
+
+  render: function(){
+    var styleObj={};
+    styleObj.backgroundColor=this.props.isCenter?'brown':'gray';
+    return(
+      <span className='ctrl-spot' style={styleObj} onClick={this.handleClick}>
+      </span>
+    )
+  }
+});
+
 var GalleryApp=React.createClass({
   Constant: {
     leftMin:0,
@@ -35,7 +55,10 @@ var GalleryApp=React.createClass({
     rightMin:0,
     rightMax:0,
     topMin:0,
-    topMax:0
+    topMax:0,
+    leftCenter:0,
+    topCenter:0,
+    stageHeight:0
   },
 
   getInitialState: function(){
@@ -48,9 +71,11 @@ var GalleryApp=React.createClass({
       }
         rotate:0,
         isInverse: false,
-        isCenter: false
+        isCenter: false,
+        zIndex: 0
         */
-      ]
+      ],
+      stageHeight:window.innerHeight
     };
   },
 
@@ -64,7 +89,12 @@ var GalleryApp=React.createClass({
       }
       imgsArrangeArr[i].pos.top=getRandomBetween(this.Constant.topMin,this.Constant.topMax);
       imgsArrangeArr[i].rotate=getRandomAngle();
+      imgsArrangeArr[i].isCenter=false;
     }
+    imgsArrangeArr[centerIndex].pos.left=this.Constant.leftCenter;
+    imgsArrangeArr[centerIndex].pos.top=this.Constant.topCenter;
+    imgsArrangeArr[centerIndex].rotate='0deg';
+    imgsArrangeArr[centerIndex].isCenter=true;
     this.setState({
       imgsArrangeArr: imgsArrangeArr
     });
@@ -83,11 +113,21 @@ var GalleryApp=React.createClass({
     this.Constant.rightMax=Math.ceil(stageWidth-imgWidth/2);
     this.Constant.topMin=Math.ceil(-imgHeight/2);
     this.Constant.topMax=Math.ceil(stageHeight-imgHeight/2);
+    this.Constant.leftCenter=Math.ceil(stageWidth/2-imgWidth/2);
+    this.Constant.topCenter=Math.ceil(stageHeight/2-imgHeight/2);
+    this.Constant.stageHeight=stageHeight;
     this.reArrange(0);
   },
 
+  center: function(i){
+    return function(){
+      this.reArrange(i);
+    }.bind(this);
+  },
+
   render: function(){
-    var ImageModules=[];
+    var ImageModules=[],
+        CtrlModules=[];
     for(var i=0;i<11;i++){
       var data={};
       data.imgUrl='./img/'+i+'.jpg';
@@ -98,15 +138,30 @@ var GalleryApp=React.createClass({
             left:0,
             top:0
           },
-          rotate:'0deg'
+          rotate:'0deg',
+          isCenter:false,
         }
       }
-      var arrange=this.state.imgsArrangeArr[i];
-      ImageModules.push(<ImageModule key={i} data={data} ref={"img"+i} arrange={arrange}/>);
+      // var center=(function(i){
+      //   return function(){
+      //     this.reArrange(i);
+      //   }.bind(this);
+      // })(i);
+      // console.log(center);
+      var styleObj={};
+      styleObj['height']=this.state.stageHeight;
+      console.log(styleObj.height);
+      ImageModules.push(<ImageModule key={i} data={data} ref={"img"+i} arrange={this.state.imgsArrangeArr[i]}/>);
+      CtrlModules.push(<CtrlModule key={i} isCenter={this.state.imgsArrangeArr[i].isCenter} reArrange={this.center(i)}/>);
     }
     return (
-      <section className='stage' ref='stage'>
-        {ImageModules}
+      <section className='stage' ref='stage' style={styleObj}>
+        <section className='img-stage'>
+          {ImageModules}
+        </section>
+        <section className='ctrl-stage'>
+          {CtrlModules}
+        </section>
       </section>
     )
   }
